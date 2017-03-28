c7ec8d537f1fad3b1038ab6b20467d8a62bc0e6e
julianoezequiel <julianoezequiel@mail.com>
2017-03-27 22:43:18 -0300

    iniit

---
+++ minhaserie/src/main/java/com/jc/MinhaserieApplication.java
@@ -0,0 +1,12 @@
+package com.jc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaserieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaserieApplication.class, args);
+	}
+}
